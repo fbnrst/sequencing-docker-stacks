16dbce7e2478c1eb673d737c0021cf46aca7863a
Fabian Rost <rost@mpi-cbg.de>
2025-05-23 14:09:23 +0000

    add fastcluster and proxyC to sequencing-base

--- tests/sequencing-base-notebook/test_packages.py
+++ tests/sequencing-base-notebook/test_packages.py
@@ -84,6 +84,7 @@ PACKAGE_MAPPING = {
     "mofa2": "MOFA2",
     "org.hs.eg.db": "org.Hs.eg.db",
     "org.mm.eg.db": "org.Mm.eg.db",
+    "proxyc": "proxyC",
     "randomforest": "randomForest",
     "rcurl": "RCurl",
     "reactomepa": "ReactomePA",
